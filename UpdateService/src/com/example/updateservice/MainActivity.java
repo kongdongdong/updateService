f7113858fbfe7df5e62f5feb848b76713798d3ae
kongdongdong <1003402938@qq.com>
2015-11-26 17:06:59 +0800

    第一次提交

--- UpdateService/src/com/example/updateservice/MainActivity.java
+++ UpdateService/src/com/example/updateservice/MainActivity.java
@@ -1,28 +1,15 @@
-# APK版本更新，软件下载更新，通知栏显示进度
+package com.example.updateservice;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 
-    
-首先你要添加权限
-
-    <uses-permission android:name="android.permission.INTERNET"/>
-    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
-    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
-
-
-然后 只要启动一个服务就可以实现软件下载更新，通知栏显示进度
-
-    package com.example.updateservice;
-    
-    import android.app.Activity;
-    import android.app.AlertDialog;
-    import android.app.AlertDialog.Builder;
-    import android.app.Dialog;
-    import android.content.Context;
-    import android.content.DialogInterface;
-    import android.content.Intent;
-    import android.os.Bundle;
-    
-    public class MainActivity extends Activity {
+public class MainActivity extends Activity {
 
 	Context mContext;
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,5 +44,4 @@
 		});
 		builder.show();
 	}
-    }
-
+}
